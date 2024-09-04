379c03f24146ebec271b8df0b35403c68298b164
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2024-09-04 13:05:40 +0530

    Fixed avatar not loading

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Container, Flex, Spinner } from "@radix-ui/themes";
+import { Avatar, Container, Flex, Spinner } from "@radix-ui/themes";
 import Link from "next/link";
 import { FcTodoList } from "react-icons/fc";
 import { IoPersonSharp } from "react-icons/io5";
@@ -31,10 +31,20 @@ const Navbar = () => {
             <Pencil2Icon className="w-5 h-5" />
           </Link>
           {status === "authenticated" && (
-            <Link href="/api/auth/signout">Log out</Link>
+            <Link href="/user">
+              <Avatar
+                src={session.user.image}
+                fallback="?"
+                radius="full"
+                size="2"
+                referrerPolicy="no-referrer"
+              />
+            </Link>
           )}
           {status === "unauthenticated" && (
-            <Link href="/api/auth/signin">Login</Link>
+            <Link href="/api/auth/signin">
+              <IoPersonSharp />
+            </Link>
           )}
           {status === "loading" && <Spinner />}
         </Flex>
